c2bf929870546a7ee5c8ec372e72136df797b486
Vasily Yakovenko <bitnikvasya@mail.ru>
2019-06-18 09:31:14 +0300

    Личный проект: пока все дома

--- js/main.js
+++ js/main.js
@@ -9,27 +9,27 @@ map.classList.remove('map--faded');
 var similarListElement = map.querySelector('.map__pins');
 var similarPinsTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-// Случайное число от 0 до длины массива
-var getRandomArray = function (array) {
-  return Math.floor(Math.random() * array.length);
-};
-
 // Случайное число от min до max
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-// Массив с объектами
-var ads = [];
+// Случайное число от 0 до длины массива
+var getRandomElement = function (array) {
+  return array[getRandomNumber(0, array.length)];
+};
 
+// Массив с объектами
 var generateAds = function () {
+  var ads = [];
+
   for (var i = 0; i < ADS_AMOUNT; i++) {
     ads[i] = {
       'author': {
         'avatar': 'img/avatars/user0' + (i + 1) + '.png'
       },
       'offer': {
-        'type': OFFER_TYPE[getRandomArray(OFFER_TYPE)]
+        'type': getRandomElement(OFFER_TYPE)
       },
       'location': {
         'x': getRandomNumber(0, 1200),
@@ -38,24 +38,20 @@ var generateAds = function () {
     };
   }
 
-  return ads[i];
+  return ads;
 };
 
-for (var j = 0; j < ADS_AMOUNT; j++) {
-  ads.push(generateAds());
-}
-
-var renderPin = function (pin) {
+var renderPin = function () {
   var pinElement = similarPinsTemplate.cloneNode(true);
-  pinElement.style.cssText = 'left: ' + (pin.location.x - 25) + 'px; top: ' + (pin.location.y - 70) + 'px;';
-  pinElement.querySelector('img').src = pin.author.avatar;
-  pinElement.querySelector('img').alt = pin.offer.type;
+  pinElement.style.cssText = 'left: ' + (generateAds[i].location.x - 25) + 'px; top: ' + (generateAds[i].location.y - 70) + 'px;';
+  pinElement.querySelector('img').src = generateAds[i].author.avatar;
+  pinElement.querySelector('img').alt = generateAds[i].offer.type;
   return pinElement;
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < ads.length; i++) {
-  fragment.appendChild(renderPin(ads[i]));
+for (var i = 0; i < generateAds.length; i++) {
+  fragment.appendChild(renderPin(generateAds[i]));
 }
 
 similarListElement.appendChild(fragment);
